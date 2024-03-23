fa42654b7661b2bc25909dadbeb727288c048bf1
Takayuki Miura <takayuki.miura28@gmail.com>
2024-03-23 21:50:13 +0900

    perf: jQueryを使わない

--- workspaces/client/src/index.tsx
+++ workspaces/client/src/index.tsx
@@ -1,6 +1,5 @@
 // import './side-effects';
 
-import $ from 'jquery';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { SWRConfig } from 'swr';
@@ -15,20 +14,22 @@ const main = async () => {
   await registerServiceWorker();
   // await preloadImages();
 
-  $(document).ready(() => {
-    if (window.location.pathname.startsWith('/admin')) {
-      ReactDOM.createRoot($('#root').get(0)!).render(<AdminApp />);
-    } else {
-      ReactDOM.hydrateRoot(
-        $('#root').get(0)!,
-        <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
-          <BrowserRouter>
-            <ClientApp />
-          </BrowserRouter>
-        </SWRConfig>,
-      );
-    }
-  });
+  const root = document.getElementById('root');
+  if (!root) {
+    throw new Error('root element not found');
+  }
+  if (window.location.pathname.startsWith('/admin')) {
+    ReactDOM.createRoot(root).render(<AdminApp />);
+  } else {
+    ReactDOM.hydrateRoot(
+      root,
+      <SWRConfig value={{ revalidateIfStale: true, revalidateOnFocus: false, revalidateOnReconnect: false }}>
+        <BrowserRouter>
+          <ClientApp />
+        </BrowserRouter>
+      </SWRConfig>,
+    );
+  }
 };
 
 main().catch(console.error);
