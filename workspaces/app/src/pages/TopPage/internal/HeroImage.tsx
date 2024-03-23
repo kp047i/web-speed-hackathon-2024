88ec44dac15c664e144e315565b2aac97ebe70c2
Takayuki Miura <takayuki.miura28@gmail.com>
2024-03-23 16:37:05 +0900

    HeroImageコンポーネントの画像サイズを変更

--- workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
+++ workspaces/app/src/pages/TopPage/internal/HeroImage.tsx
@@ -13,7 +13,12 @@ const _Image = styled.img`
 export const HeroImage: React.FC = () => {
   return (
     <_Wrapper>
-      <_Image alt="Cyber TOON" src="https://res.cloudinary.com/dlibdyano/image/upload/v1711166854/hero.webp" />
+      <_Image
+        alt="Cyber TOON"
+        height={576}
+        src="https://res.cloudinary.com/dlibdyano/image/upload/v1711166854/hero.webp"
+        width={1024}
+      />
     </_Wrapper>
   );
 };
